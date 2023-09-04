2520605c29c8a49fc03736f5d6b06b8d44df8579
Serhii Ihnatiev <141933729+iamihnatiev@users.noreply.github.com>
2023-09-04 16:18:51 +0300

    Add tiny changes

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -2,9 +2,13 @@ from aiogram import Router, html
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from src.db import Database
+
 start_router = Router(name='start')
 
 
 @start_router.message(CommandStart())
-async def start_handler(message: Message):
+async def start_handler(message: Message, db: Database):
+    await db.user.find(ident=message.from_user.id)
+
     await message.answer(text=f"Hello, {html.bold(message.from_user.first_name)}!")
--- src/bot/middlewares/database.py
+++ src/bot/middlewares/database.py
@@ -1,5 +1,5 @@
 from collections.abc import Callable, Awaitable
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,7 +14,7 @@ class DatabaseMiddleware(BaseMiddleware):
     async def __call__(
         self,
         handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-        event: Message | CallbackQuery,
+        event: Union[Message, CallbackQuery],
         data: DatabaseDTO,
     ) -> Any:
         async with AsyncSession(bind=data['engine']) as session:
