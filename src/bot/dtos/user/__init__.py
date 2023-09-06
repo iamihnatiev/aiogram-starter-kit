9f52cfd87f5df2e002b95bd22e2132d330c83549
Serhii Ihnatiev <141933729+iamihnatiev@users.noreply.github.com>
2023-09-06 17:21:28 +0300

    Add tiny fix

---
+++ src/bot/dtos/user/__init__.py
@@ -0,0 +1,9 @@
+from .user_dto import UserDTO
+from .user_create_dto import UserCreateDTO
+from .user_update_dto import UserUpdateDTO
+
+__all__ = (
+    'UserDTO',
+    'UserCreateDTO',
+    'UserUpdateDTO',
+)
